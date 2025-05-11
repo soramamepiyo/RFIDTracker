083932c5f8d8d04022a713f8b10010bc77e12390
中西空 <sora10201020@gmail.com>
2025-05-11 11:57:49 +0900

    arudinoの制御ファイルを追加

---
+++ aruduino/RFIDReader.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN_1 7  // リーダー1のSDA（SS）ピン
+#define SS_PIN_2 6   // リーダー2のSDA（SS）ピン
+#define SS_PIN_3 5   // リーダー3のSDA（SS）ピン
+#define RST_PIN 9    // RSTは共通
+
+MFRC522 rfid1(SS_PIN_1, RST_PIN);
+MFRC522 rfid2(SS_PIN_2, RST_PIN);
+MFRC522 rfid3(SS_PIN_3, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  rfid1.PCD_Init();
+  rfid2.PCD_Init();
+  rfid3.PCD_Init();
+  Serial.println("Ready to read from 3 RC522 readers.");
+}
+
+void loop() {
+  // リーダー1
+  if (rfid1.PICC_IsNewCardPresent() && rfid1.PICC_ReadCardSerial()) {
+    Serial.print("1:");
+    printUID(rfid1);
+    rfid1.PICC_HaltA();
+    rfid1.PCD_StopCrypto1();
+    delay(1000);
+  }
+
+  // リーダー2
+  if (rfid2.PICC_IsNewCardPresent() && rfid2.PICC_ReadCardSerial()) {
+    Serial.print("2:");
+    printUID(rfid2);
+    rfid2.PICC_HaltA();
+    rfid2.PCD_StopCrypto1();
+    delay(1000);
+  }
+
+  // リーダー3
+  if (rfid3.PICC_IsNewCardPresent() && rfid3.PICC_ReadCardSerial()) {
+    Serial.print("3:");
+    printUID(rfid3);
+    rfid3.PICC_HaltA();
+    rfid3.PCD_StopCrypto1();
+    delay(1000);
+  }
+}
+
+void printUID(MFRC522 &reader) {
+  for (byte i = 0; i < reader.uid.size; i++) {
+    Serial.print(reader.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(reader.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+}
